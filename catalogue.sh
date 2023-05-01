884e6b1131c6eb627a7f326d05020dfca422d89d
EPAPRA <dave.devops109@gmail.com>
2023-05-02 03:13:49 +0530

    Catalogue scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,12 @@
 script_location=$(pwd)
+set -e
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-useradd roboshop
+#useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
