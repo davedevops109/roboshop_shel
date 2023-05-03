abe6191fc8981aec3e03253a2dbc08982915e45c
EPAPRA <dave.devops109@gmail.com>
2023-05-03 22:29:42 +0530

    Creating servers and route

--- common.sh
+++ common.sh
@@ -3,10 +3,10 @@ LOG=/tmp/roboshop.log
 
 status_check() {
  if [ $? -eq 0 ]; then
-   echo -e "\e[032msuccess \e[0m"
+   echo -e "\e[1;032msuccess\e[0m"
  else
-   echo -e "\e[031mfailure \e[0m"
-   echo "Refer log file for more information, LOG - S{LOG}"
+   echo -e "\e[1;031mfailure\e[0m"
+   echo "Refer log file for more information, LOG - ${LOG}"
    exit
  fi
 }
