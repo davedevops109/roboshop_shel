82d2985f61c3f96bc9564fcd920814c94b4b3f66
EPAPRA <dave.devops109@gmail.com>
2023-05-04 01:22:13 +0530

    Creating redis

--- redis.sh
+++ redis.sh
@@ -13,7 +13,7 @@ yum install redis -y &>>${LOG}
 status_check
 
 print_head "update redis listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf /etc/redis.conf &>>${LOG}
 status_check
 
 print_head "Enable redis"
