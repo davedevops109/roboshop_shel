3be39daaf319eeceef21ed06f2ebbf1bc6b15c8c
EPAPRA <dave.devops109@gmail.com>
2023-05-03 21:43:11 +0530

    Creating servers and route

--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,31 @@
 source common.sh
 
-print_head " Install Nginx"
+print_head "Install Nginx"
 yum install nginx -y &>>${LOG}
 status_check
 
-print_head " Remove nginx old content"
+print_head "Remove nginx old content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
 status_check
 
-print_head " download frontend content"
+print_head "download frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 status_check
 
 cd /usr/share/nginx/html &>>${LOG}
 
-print_head " extract frontend content"
+print_head "extract frontend content"
 unzip /tmp/frontend.zip &>>${LOG}
 status_check
 
-print_head " copy roboshop nginx config file"
+print_head "copy roboshop nginx config file"
 cp ${script_location}/file/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 status_check
 
-print_head " Enable Nginx"
+print_head "Enable Nginx"
 systemctl enable nginx &>>${LOG}
 status_check
 
-print_head " start Nginx"
+print_head "start Nginx"
 systemctl start nginx &>>${LOG}
 status_check
