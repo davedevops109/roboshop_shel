ce54dce250b0cb8ae5a808a64230704b2780cd5e
EPAPRA <dave.devops109@gmail.com>
2023-05-03 21:31:28 +0530

    Creating servers and route

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,10 @@ yum install nodejs -y &>>${LOG}
 status_check
 
 print_head -e "Add application user"
-useradd roboshop &>>${LOG}
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
 status_check
 
 mkdir -p /app &>>${LOG}
