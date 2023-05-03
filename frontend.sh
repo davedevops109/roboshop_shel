4d393a7913181e44d5db0227391447c04cd8b1b8
EPAPRA <dave.devops109@gmail.com>
2023-05-03 17:03:09 +0530

    Creating servers and route

--- frontend.sh
+++ frontend.sh
@@ -3,22 +3,30 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
+echo $?
 
 echo -e "\e[35m Remove nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+echo $?
 
 echo -e "\e[35m download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+echo $?
+
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m extract frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
+echo $?
 
 echo -e "\e[35m copy roboshop nginx config file\e[0m"
 cp ${script_location}/file/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
 
 echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
+echo $?
 
 echo -e "\e[35m start Nginx\e[0m"
 systemctl start nginx &>>${LOG}
+echo $?
